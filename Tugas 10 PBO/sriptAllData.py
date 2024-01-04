d0264cec730424dd85c0c6f5fa4f23ac4cbcf781
IbnuAlwan017 <148968744+IbnuAlwan017@users.noreply.github.com>
2024-01-04 23:16:01 +0700

    Delete Tugas 10 PBO directory

--- Tugas 10 PBO/sriptAllData.py
+++
@@ -1,6 +0,0 @@
-from matakuliah import Matakuliah
-
-# Tampilan Data
-A = Matakuliah()
-B = A.getAllData()
-print(B)
--- Tugas 10 PBO/sriptDelete.py
+++
@@ -1,9 +0,0 @@
-from matakuliah import Matakuliah
-
-# Delete Data
-A = Matakuliah()
-kodemk = "1001"
-
-A.deleteByKodeMK(kodemk)
-B = A.getAllData()
-print(B)
